02f77b41f8e8246385666096ca52b2f35d59418c
Jordan Lund <jordan.l.lund@gmail.com>
2015-03-27 01:54:11 -0700

    Revert "Bug 1131611: remove android SDK 18; r=Callek"
    This reverts commit 4f42804be44e7e6c9bdaf87e5ce8fa4bb0f2bc8c.

--- modules/talos/manifests/init.pp
+++ modules/talos/manifests/init.pp
@@ -41,6 +41,7 @@ class talos {
                     include packages::cpu-checker
                     include packages::qemu-kvm
                     include packages::bridge-utils
+                    include androidemulator
                 }
             }
         }
